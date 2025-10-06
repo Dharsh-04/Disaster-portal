fe2fde7c71f45b0ca92c18f5bb80998f4c014c63
Dharshni R <dharshnich04@gmail.com>
2025-10-06 21:33:32 +0530

    Auto-create tables on startup

--- app.py
+++ app.py
@@ -204,6 +204,9 @@ def ngo_logout():
 # ----------------------------------------------------
 # RUN APP
 # ----------------------------------------------------
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
